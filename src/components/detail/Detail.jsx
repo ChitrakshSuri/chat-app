c4c29a8bc1de8c6b313468e1b3fc63f89d91ac9f
Chitraksh Suri <chitrakshsuri2005@gmail.com>
2025-02-07 14:38:35 +0530

    comleted frontend

--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -1,10 +1,75 @@
-import React from 'react'
-import "./detail.css"
+import "./detail.css";
 
 const Detail = () => {
   return (
-    <div className='detail'>Detail</div>
-  )
-}
+    <div className="detail">
+      <div className="user">
+        <img src="./avatar.png" alt="" />
+        <h2>Shourya Suri</h2>
+        <p>Lorem ipsum dolor sit.</p>
+      </div>
+      <div className="info">
+        <div className="option">
+          <div className="title">
+            <span>Chat Setting</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Privacy & help</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Shared Photos</span>
+            <img src="./arrowDown.png" alt="" />
+          </div>
+          <div className="photos">
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img
+                  src="https://www.pexels.com/photo/snowy-winter-wonderland-in-lapland-finland-30433172/"
+                  alt="chhoti tasveer"
+                />
+                <span>photo_2024-01.png</span>
+              </div>
+              <img src="./download.png" alt="" className="icon" />
+            </div>
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img
+                  src="https://www.pexels.com/photo/snowy-winter-wonderland-in-lapland-finland-30433172/"
+                  alt="chhoti tasveer"
+                />
+                <span>photo_2024-01.png</span>
+              </div>
+              <img src="./download.png" alt="" className="icon" />
+            </div>
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img
+                  src="https://www.pexels.com/photo/snowy-winter-wonderland-in-lapland-finland-30433172/"
+                  alt="chhoti tasveer"
+                />
+                <span>photo_2024-01.png</span>
+              </div>
+              <img src="./download.png" alt="" className="icon" />
+            </div>
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Shared Files</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <button>Block User</button>
+        <button className="logout">Logout</button>
+      </div>
+    </div>
+  );
+};
 
-export default Detail
+export default Detail;
