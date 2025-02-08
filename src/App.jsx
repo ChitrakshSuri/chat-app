4993e8c39efd65a1b3eb6350c9d8e2c82f85e53d
Chitraksh Suri <chitrakshsuri2005@gmail.com>
2025-02-08 12:36:46 +0530

    Revert "Store the User"
    This reverts commit 92b2e3be4e73375d24755ff549531db3feb82c9e.

--- src/App.jsx
+++ src/App.jsx
@@ -6,24 +6,19 @@ import Notification from "./components/notification/notification";
 import { useEffect } from "react";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "./lib/firebase";
-import { useUserStore } from "./lib/userStore";
 
 const App = () => {
-  const { currentUser, isLoading, fetchUserInfo } = useUserStore(); // ✅ Correct usage
-
+  const user = false;
   useEffect(() => {
     const unSub = onAuthStateChanged(auth, (user) => {
-      fetchUserInfo(user?.uid);
+      console.log(user);
+      
     });
 
     return () => {
       unSub();
     };
-  }, [fetchUserInfo]);
-
-  console.log(currentUser);
-
-  if (isLoading) return <div className="loading">Loading...</div>;
+  }, []);
 
   return (
     <div className="container">
